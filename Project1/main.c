be45d77728e5ffa196c33d7945cb2b19aca7d178
calisthak <d1292605@o365.fcu.edu.tw>
2025-05-15 11:27:37 +0800

    lesson 4

--- Project1/main.c
+++ Project1/main.c
@@ -7,6 +7,23 @@
 #define CANVAS_WIDTH 10
 #define CANVAS_HEIGHT 20
 
+#define FALL_DELAY 500
+#define RENDER_DELAY 100
+
+#define LEFT_KEY 0x25
+#define RIGHT_KEY 0x27 
+#define ROTATE_KEY 0x26 
+#define DOWN_KEY 0x28 
+#define FALL_KEY 0x20 
+
+#define LEFT_FUNC() GetAsyncKeyState(LEFT_KEY) & 0x8000
+#define RIGHT_FUNC() GetAsyncKeyState(RIGHT_KEY) & 0x8000
+#define ROTATE_FUNC() GetAsyncKeyState(ROTATE_KEY) & 0x8000
+#define DOWN_FUNC() GetAsyncKeyState(DOWN_KEY) & 0x8000
+#define FALL_FUNC() GetAsyncKeyState(FALL_KEY) & 0x8000
+
+
+
 typedef enum
 {
     EMPTY = -1,
@@ -290,15 +307,20 @@ void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
         for (int j = 0; i < 4; i++) {
             printf("\033[%d;%dHNext:", i * 4 + j, CANVAS_WIDTH * 2 + 15);
             for (int k = 0; k < 4; k++) {
-                if(j < shapeData.size && k < shapeData.size && shapeData.rotates[0][j][k]){
+                if (j < shapeData.size && k < shapeData.size && shapeData.rotates[0][j][k]) {
                     printf("\x1b[%dm  ", shapeData.color);
-                }else{
+                }
+                else {
                     printf("\x1b[0m  ");
+                }
+            }
         }
     }
+    return 0;
 }
 
-        bool move(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int originalX, int originalY, int originalRotate, int newX, int newY, int newRotate, ShapeId shapeId) {
+bool move(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], int originalX, int originalY, int originalRotate, int newX, int newY, int newRotate, ShapeId shapeId)
+{
     Shape shapeData = shapes[shapeId];
     int size = shapeData.size;
 
@@ -308,7 +330,7 @@ void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
                 if (newX + j < 0 || newX + j >= CANVAS_WIDTH || newY + i < 0 || newY + i >= CANVAS_HEIGHT) {
                     return false;
                 }
-                if(!canvas[newY + i][newX + j]).current&& canvas[newY + i][newX + j].shape != EMPTY){
+                if (!canvas[newY + i][newX + j].current && canvas[newY + i][newX + j].shape != EMPTY) {
                     return false;
                 }
             }
@@ -317,8 +339,9 @@ void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
 
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            if (shapeData.rotates[originalRotate][i][j])
+            if (shapeData.rotates[originalRotate][i][j]){
                 resetBlock(&canvas[originalY + i][originalX + j]);
+            }
         }
     }
     
@@ -332,23 +355,97 @@ void printCanvas(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
     return true;
 }
 
-
-void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State *state) {
-    if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0]))
-        state->y++;
-    else {
-        state->score += clearLine(canvas);
-
-        state->x = CANVAS_WIDTH / 2;
-        state->y = 0;
-        state->rotate = 0;
-        state->falltime = 0;
-        state->queue[0] = state->queue[1];
-        state->queue[1] = state->queue[2];
-        state->queue[2] = state->queue[3];
-        state->queue[3] = rand() % 7;
+int clearLine(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH])
+{
+    for (int i = 0; i < CANVAS_HEIGHT; i++)
+    {
+        for (int j = 0; j < CANVAS_WIDTH; j++)
+        {
+            if (canvas[i][j].current)
+            {
+                canvas[i][j].current = false;
+            }
+        }
     }
 
+    int linesCleared = 0;
+    for (int i = CANVAS_HEIGHT - 1; i >= 0; i--)
+    {
+        bool isFull = true;
+        for (int j = 0; j < CANVAS_WIDTH; j++)
+        {
+            if (canvas[i][j].shape == EMPTY) {
+                isFull = false;
+                break;
+            }
+        }
+        if (isFull) {
+            linesCleared += 1;
+
+            for (int j = i; j > 0; j--)
+            {
+                for (int k = 0; k < CANVAS_WIDTH; k++)
+                {
+                    setBlock(&canvas[j][k], canvas[j - 1][k].color, canvas[j - 1][k].shape, false);
+                    resetBlock(&canvas[j - 1][k]);
+                }
+            }
+            i++;
+        }
+    }
+
+
+    return linesCleared;
+}
+
+
+void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state) {
+    
+    if (ROTATE_FUNC()) {
+        int newRotate = (state->rotate + 1) % 4;
+        if (move(canvas, state->x, state->y, state->rotate, state->x, state->y, newRotate, state->queue[0]))
+            state->rotate = newRotate;
+    }
+    else if (LEFT_FUNC()) {
+        if (move(canvas, state->x, state->y, state->rotate, state->x - 1, state->y, state->rotate, state->queue[0]))
+            state->x -= 1;
+    }
+    else if (RIGHT_FUNC()) {
+        if (move(canvas, state->x, state->y, state->rotate, state->x + 1, state->y, state->rotate, state->queue[0]))
+            state->x += 1;
+    }
+    else if (DOWN_FUNC()) {
+        state->fallTime = FALL_DELAY;
+    }
+    else if (FALL_FUNC()) {
+        state->fallTime += FALL_DELAY * CANVAS_HEIGHT;
+    }
+    state->fallTime += RENDER_DELAY;
+
+
+
+    while (state->fallTime >= FALL_DELAY) {
+        if (move(canvas, state->x, state->y, state->rotate, state->x, state->y + 1, state->rotate, state->queue[0]))
+            state->y++;
+        else {
+            state->score += clearLine(canvas);
+
+            state->x = CANVAS_WIDTH / 2;
+            state->y = 0;
+            state->rotate = 0;
+            state->fallTime = 0;
+            state->queue[0] = state->queue[1];
+            state->queue[1] = state->queue[2];
+            state->queue[2] = state->queue[3];
+            state->queue[3] = rand() % 7;
+
+            if (!move(canvas, state->x, state->y, state->rotate, state->x, state->y, state->rotate, state->queue[0]))
+            {
+                printf("\033[%d;%dH\x1b[41m GAME OVER \x1b[0m\033[%d;%dH", CANVAS_HEIGHT - 3, CANVAS_WIDTH * 2 + 5, CANVAS_HEIGHT + 5, 0);
+                exit(0);
+            }
+        }
+    }
     return;
 }
 
